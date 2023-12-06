3264d6dcb0f5f518173ec2fc430013ab5adf86b6
Iman Anooshehpour <imananoosheh@gmail.com>
2023-12-05 19:05:17 -0800

    completed, ready for automation

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
+const fileSystem = require("fs");
+
 async function getContributions(token, username) {
-    const headers = {
-        'Authorization': `bearer ${token}`,
-    }
-    const body = {
-        "query": `query {
+  const headers = {
+    Authorization: `bearer ${token}`,
+  };
+  const body = {
+    query: `query {
             user(login: "${username}") {
               name
               contributionsCollection {
@@ -22,12 +25,37 @@ async function getContributions(token, username) {
                 }
               }
             }
-          }`
+          }`,
+  };
+  const response = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: headers,
+  });
+  const data = await response.json();
+  fileSystem.writeFile("./archive.json", JSON.stringify(data), (error) => {
+    if (error) {
+      console.log(error);
     }
-    const response = await fetch('https://api.github.com/graphql', { method: 'POST', body: JSON.stringify(body), headers: headers })
-    const data = await response.json()
-    return data
+  });
 }
 
-const data = await getContributions('token', 'username')
-console.log(data)
+getContributions(process.env.GITHUB_ACCESS_TOKEN, process.env.GITHUB_USERNAME);
+
+fileSystem.readFile('./archive.json', 'utf-8', (err, data)=>{
+  if(err){
+    console.log(err)
+    return
+  }
+  const contributions = JSON.parse(data)
+  let contributionFile = []
+  for(const week of contributions.data.user.contributionsCollection.contributionCalendar.weeks){
+    contributionFile.push(...week["contributionDays"])
+  }
+  fileSystem.writeFile('./gh-contributions.json', JSON.stringify(contributionFile), err => {
+    if(err){
+      console.log(err)
+      return
+    }
+  })
+})
