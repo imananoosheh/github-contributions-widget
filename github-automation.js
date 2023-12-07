2bae929729322cf580bf89d125dbfa7c333e8e1d
Iman Anooshehpour <imananoosheh@gmail.com>
2023-12-07 11:50:00 -0800

    added origin and main branch name in git push command

--- github-automation.js
+++ github-automation.js
@@ -2,4 +2,4 @@ const { spawn } = require('node:child_process');
 const git_stag = spawn('git', ['add', 'gh-contributions.json'],{cwd: `${process.env.GITHUB_CONTRIBUTION_WORKING_DIR}`});
 const today = new Date().toISOString()
 const git_commit = spawn('git', ['commit', '-m', `${today}_github_contributions`],{cwd: `${process.env.GITHUB_CONTRIBUTION_WORKING_DIR}`})
-const git_push = spawn('git', ['push'],{cwd: `${process.env.GITHUB_CONTRIBUTION_WORKING_DIR}`})
+const git_push = spawn('git', ['push', 'origin', 'main'],{cwd: `${process.env.GITHUB_CONTRIBUTION_WORKING_DIR}`})
