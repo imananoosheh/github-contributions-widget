f59ad3ec1d315a1e8769ea032245d8536df71fc8
Iman Anooshehpour <imananoosheh@gmail.com>
2023-12-21 18:39:54 -0800

    cleanup some indentation in GraphQL query, better readability

--- server.js
+++ server.js
@@ -35,25 +35,25 @@ async function getContributions(token, username) {
 	// TODO:    fix indentation
 	const body = {
 		query: `query {
-              user(login: "${username}") {
-                name
+			user(login: "${username}") {
+            	name
                 contributionsCollection {
-                  contributionCalendar {
-                    colors
-                    totalContributions
-                    weeks {
-                      contributionDays {
-                        color
-                        contributionCount
-                        date
-                        weekday
-                      }
-                      firstDay
-                    }
-                  }
+                	contributionCalendar {
+						colors
+						totalContributions
+						weeks {
+                    		contributionDays {
+								color
+								contributionCount
+								date
+								weekday
+                    		}
+                    		firstDay
+                    	}
+                	}
                 }
-              }
-            }`,
+            }
+		}`,
 	};
 	const response = await fetch("https://api.github.com/graphql", {
 		method: "POST",
