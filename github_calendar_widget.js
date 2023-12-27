41e498f1b120fd8cb551460ca1887389cb45c710
Iman Anooshehpour <imananoosheh@gmail.com>
2023-12-27 14:08:17 -0800

    spliting a single script to two front and back codes

---
+++ github_calendar_widget.js
@@ -0,0 +1,221 @@
+// github_calendar_widget.js
+const monthsMap = {
+	0: "Jan",
+	1: "Feb",
+	2: "Mar",
+	3: "Apr",
+	4: "May",
+	5: "Jun",
+	6: "Jul",
+	7: "Aug",
+	8: "Sep",
+	9: "Oct",
+	10: "Nov",
+	11: "Dec",
+};
+
+function renderCalendar(contributionData) {
+	const startingMonth = new Date(contributionData[0]["date"]).getMonth();
+	const calendarComponent = document.getElementById("calendar-component");
+	const calendarHeader = document.createElement("h1");
+	calendarHeader.textContent = "GitHub Activity Calendar";
+	calendarComponent.append(calendarHeader);
+	const calendarMonthsTemplate = document.createElement("div");
+	calendarMonthsTemplate.className = "months";
+	for (let i = 0; i < 13; i++) {
+		const month = document.createElement("div");
+		month.className = "month";
+		const currentMonth =
+			i + startingMonth > 11 ? i + startingMonth - 12 : i + startingMonth;
+		month.textContent = `${monthsMap[currentMonth]}`;
+		calendarMonthsTemplate.append(month);
+	}
+	calendarComponent.append(calendarMonthsTemplate);
+	const calendarDaysTemplate = document.createElement("div");
+	calendarDaysTemplate.className = "calendar-wrapper";
+	const calendarDaysAsideBlock = document.createElement("aside");
+	const daysOfWeeks = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+	for (let i = 0; i < 7; i++) {
+		const day = document.createElement("div");
+		day.textContent = `${daysOfWeeks[i]}`;
+		calendarDaysAsideBlock.append(day);
+	}
+	calendarDaysTemplate.append(calendarDaysAsideBlock);
+	const calendar = document.createElement("div");
+	calendar.id = "calendar";
+	calendar.innerHTML = "";
+	calendarDaysTemplate.append(calendar);
+	calendarComponent.append(calendarDaysTemplate);
+
+	// Generate calendar grid
+	for (let dayOfWeek = 0; dayOfWeek < 7; dayOfWeek++) {
+		for (let week = 0; week < 53; week++) {
+			const dayElement = document.createElement("div");
+			dayElement.classList.add("day");
+			const currentDate = new Date();
+			currentDate.setDate(
+				currentDate.getDate() - 365 + (dayOfWeek + week * 7)
+			);
+
+			const currentISODate = currentDate.toISOString().split("T")[0];
+			const data = contributionData.find(
+				(entry) => entry.date === currentISODate
+			);
+			//Add gradiant proportionate to contribution count
+			if (data && data.contributionCount > 0) {
+				const colorIntensity = data.contributionCount / 10; // Adjust color intensity based on contributionCount
+				dayElement.setAttribute(
+					"style",
+					`background-color:rgba(0, 255, 0, ${colorIntensity}`
+				); // Use color from data or default color
+			}
+			dayElement.setAttribute(
+				"date",
+				currentDate.toLocaleDateString("en-US", {
+					month: "short",
+					day: "2-digit",
+				})
+			);
+			if (data) {
+				dayElement.setAttribute(
+					"contributions",
+					data.contributionCount
+				);
+				if (data.contributionCount >= 10) {
+					dayElement.textContent = "+";
+				}
+			}
+			const today = new Date();
+			if (currentDate > today) {
+				dayElement.style.backgroundColor = "transparent";
+				dayElement.style.border = "0px";
+			}
+			calendar.appendChild(dayElement);
+		}
+	}
+
+	const styles = `
+  body{
+	background-color: #121212;
+  	color: #0f0;
+  }
+  #calendar {
+    display: grid;
+    /* 53 columns for each week in a year */
+    grid-template-columns: repeat(53, 17px);
+    /* 7 rows for each day of the week */
+    grid-template-rows: repeat(7, 17px);
+    gap: 2px;
+  }
+  
+  .day {
+    width: 17px;
+    height: 17px;
+    border: 1px solid rgba(0,255,0,0.25);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    position: relative;
+    background-color: rgba(0, 0, 0, 0.5);
+	color: white;
+  }
+  .day:hover::after {
+    content: attr(contributions) " contributions on " attr(date);
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    transform: translate(-50%, -60px);
+    background-color: rgba(255, 255, 255, 0.9);
+    border: 1px solid #ccc;
+    padding: 5px;
+    border-radius: 5px;
+    white-space: nowrap;
+    color: #121212;
+    z-index: 5;
+  }
+  .calendar-wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  .calendar-wrapper aside {
+    padding-right: 1rem;
+    text-align: right;
+    width: 2rem;
+  }
+  .months {
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    margin-left: 3rem;
+  }
+  .month {
+    width: 4.75rem;
+	text-align: center;
+  }
+  #calendar-component{
+    margin: 3rem 0;
+    font-size: 1rem;
+	width: fit-content;
+  }
+  #calendar-component > h1{
+    margin-bottom: 0.5rem;
+    text-align: center;
+  }
+  `;
+	// Apply styles to the document
+	const styleElement = document.createElement("style");
+	styleElement.textContent = styles;
+	document.head.appendChild(styleElement);
+}
+
+//fetching data from API
+async function fetchDataFromServer(username) {
+	const response = await fetch(
+		`https://nulljuju.dev/github_calendar/${username}`,
+		{
+			method: "GET", // *GET, POST, PUT, DELETE, etc.
+			mode: "cors", // no-cors, *cors, same-origin
+			cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+			credentials: "same-origin", // include, *same-origin, omit
+			headers: {
+				"Content-Type": "application/json",
+				// 'Content-Type': 'application/x-www-form-urlencoded',
+			},
+		}
+	);
+	const data = await response.json();
+	if (data && response.status === 200) {
+		return data;
+	} else {
+		console.error("Error in fetching GitHub data from server");
+	}
+}
+
+// Call this function to fetch data and generate the calendar on the frontend
+async function initGitHubCalendar(username) {
+	try {
+		const contributionData = await fetchDataFromServer(username);
+		if (!contributionData || contributionData.length === 0) {
+			console.error("No contribution data available.");
+			// Handle the error or do something else
+			return;
+		}
+
+		renderCalendar(contributionData);
+	} catch (error) {
+		console.error("Error fetching contribution data:", error);
+		// Handle the error or do something else
+	}
+}
+
+const username = calendarComponent.getAttribute("username");
+if (username.length > 0) {
+	initGitHubCalendar(username);
+} else {
+	console.error(
+		"Username was not provided!\n",
+		`username fetched:${username}\n`
+	);
+}
--- server.js
+++ server.js
@@ -1,38 +1,17 @@
 // server.js
 "use strict";
-import jsdom from "jsdom";
 import dotenv from "dotenv";
 import express from "express";
 const app = express();
 dotenv.config();
 
-const { JSDOM } = jsdom;
-const dom = new JSDOM(`<!DOCTYPE html>`);
-
-const document = dom.window.document;
-
 const accessToken = process.env.GITHUB_ACCESS_TOKEN;
 
-const monthsMap = {
-	0: "Jan",
-	1: "Feb",
-	2: "Mar",
-	3: "Apr",
-	4: "May",
-	5: "Jun",
-	6: "Jul",
-	7: "Aug",
-	8: "Sep",
-	9: "Oct",
-	10: "Nov",
-	11: "Dec",
-};
 
 async function getContributions(token, username) {
 	const headers = {
 		Authorization: `bearer ${token}`,
 	};
-	// TODO:    fix indentation
 	const body = {
 		query: `query {
 			user(login: "${username}") {
@@ -70,165 +49,6 @@ async function getContributions(token, username) {
 	return contributionData;
 }
 
-function generateCalendar(contributionData) {
-	const startingMonth = new Date(contributionData[0]["date"]).getMonth();
-	// component creation steps happends here
-	const calendarComponent = document.createElement("div");
-	calendarComponent.id = "calendar-component";
-	const calendarHeader = document.createElement("h1");
-	calendarHeader.textContent = "GitHub Activity Calendar";
-	calendarComponent.append(calendarHeader);
-	const calendarMonthsTemplate = document.createElement("div");
-	calendarMonthsTemplate.className = "months";
-	for (let i = 0; i < 13; i++) {
-		const month = document.createElement("div");
-		month.className = "month";
-		const currentMonth =
-			i + startingMonth > 11 ? i + startingMonth - 12 : i + startingMonth;
-		month.textContent = `${monthsMap[currentMonth]}`;
-		calendarMonthsTemplate.append(month);
-	}
-	calendarComponent.append(calendarMonthsTemplate);
-	const calendarDaysTemplate = document.createElement("div");
-	calendarDaysTemplate.className = "calendar-wrapper";
-	const calendarDaysAsideBlock = document.createElement("aside");
-	const daysOfWeeks = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
-	for (let i = 0; i < 7; i++) {
-		const day = document.createElement("div");
-		day.textContent = `${daysOfWeeks[i]}`;
-		calendarDaysAsideBlock.append(day);
-	}
-	calendarDaysTemplate.append(calendarDaysAsideBlock);
-	const calendar = document.createElement("div");
-	calendar.id = "calendar";
-	calendar.innerHTML = "";
-	calendarDaysTemplate.append(calendar);
-	calendarComponent.append(calendarDaysTemplate);
-
-	// Generate calendar grid
-	for (let dayOfWeek = 0; dayOfWeek < 7; dayOfWeek++) {
-		for (let week = 0; week < 53; week++) {
-			const dayElement = document.createElement("div");
-			dayElement.classList.add("day");
-			const currentDate = new Date();
-			currentDate.setDate(
-				currentDate.getDate() - 365 + (dayOfWeek + week * 7)
-			);
-
-			const currentISODate = currentDate.toISOString().split("T")[0];
-			const data = contributionData.find(
-				(entry) => entry.date === currentISODate
-			);
-			//Add gradiant proportionate to contribution count
-			if (data && data.contributionCount > 0) {
-				const colorIntensity = data.contributionCount / 10; // Adjust color intensity based on contributionCount
-				dayElement.setAttribute(
-					"style",
-					`background-color:rgba(0, 255, 0, ${colorIntensity}`
-				); // Use color from data or default color
-			}
-			dayElement.setAttribute(
-				"date",
-				currentDate.toLocaleDateString("en-US", {
-					month: "short",
-					day: "2-digit",
-				})
-			);
-			if (data) {
-				dayElement.setAttribute(
-					"contributions",
-					data.contributionCount
-				);
-				if (data.contributionCount >= 10) {
-					dayElement.textContent = "+";
-				}
-			}
-			const today = new Date();
-			if (currentDate > today) {
-				dayElement.style.backgroundColor = "transparent";
-				dayElement.style.border = "0px";
-			}
-			calendar.appendChild(dayElement);
-		}
-	}
-
-	const styles = `
-  body{
-	background-color: #121212;
-  	color: #0f0;
-  }
-  #calendar {
-    display: grid;
-    /* 53 columns for each week in a year */
-    grid-template-columns: repeat(53, 17px);
-    /* 7 rows for each day of the week */
-    grid-template-rows: repeat(7, 17px);
-    gap: 2px;
-  }
-  
-  .day {
-    width: 17px;
-    height: 17px;
-    border: 1px solid rgba(0,255,0,0.25);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    position: relative;
-    background-color: rgba(0, 0, 0, 0.5);
-	color: white;
-  }
-  .day:hover::after {
-    content: attr(contributions) " contributions on " attr(date);
-    position: absolute;
-    top: 100%;
-    left: 50%;
-    transform: translate(-50%, -60px);
-    background-color: rgba(255, 255, 255, 0.9);
-    border: 1px solid #ccc;
-    padding: 5px;
-    border-radius: 5px;
-    white-space: nowrap;
-    color: #121212;
-    z-index: 5;
-  }
-  .calendar-wrapper {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-  }
-  .calendar-wrapper aside {
-    padding-right: 1rem;
-    text-align: right;
-    width: 2rem;
-  }
-  .months {
-    display: flex;
-    flex-direction: row;
-    justify-content: start;
-    margin-left: 3rem;
-  }
-  .month {
-    width: 4.75rem;
-	text-align: center;
-  }
-  #calendar-component{
-    margin: 3rem 0;
-    font-size: 1rem;
-	width: fit-content;
-  }
-  #calendar-component > h1{
-    margin-bottom: 0.5rem;
-    text-align: center;
-  }
-  `;
-	document.body.appendChild(calendarComponent);
-	// Apply styles to the document
-	const styleElement = document.createElement("style");
-	styleElement.textContent = styles;
-	document.head.appendChild(styleElement);
-}
-
 // Run Server
 
 app.get("/github_calendar/:username", async (req, res) => {
@@ -241,9 +61,8 @@ app.get("/github_calendar/:username", async (req, res) => {
 			// Handle the error, return or do something else
 			return res.status(404).send("No contribution data available.");
 		}
-		generateCalendar(contributionData);
 		// Return the result to the user
-		res.send(dom.serialize());
+		res.send(JSON.stringify(contributionData));
 		document.body.innerHTML = "";
 	} catch (error) {
 		console.error("Error fetching contribution data:", error);
