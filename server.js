e25e7dbdd697aceb99b793b73fe377198cb55144
Iman Anooshehpour <imananoosheh@gmail.com>
2024-02-06 12:28:50 -0800

    added allowed method in header

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use("/files", express.static("public"));
 app.use(function (req, res, next) {
 	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Methods", "GET");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST");
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 	res.setHeader("Access-Control-Allow-Credentials", true);
 	console.log(`Received ${req.method} request to ${req.url}`);
