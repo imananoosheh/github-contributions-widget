ca5fde8528a0252f6945fd3e2d86292c6a12bb90
Iman Anooshehpour <imananoosheh@gmail.com>
2024-06-03 14:58:23 -0700

    refactored code

--- install.sh
+++ install.sh
@@ -8,24 +8,6 @@ function customLog() {
 
 customLog "Installing GitHub Calendar Widget..."
 
-github_token=''
-read -p "Enter your GitHub access token: " github_token
-server_port=3003
-read -e -p "Enter server port (Enter to accept the default): " -i "$server_port" server_port
-customLog "Server port is set to: $server_port"
-static_directory='/usr/src/app/public/'
-read -e -p "Enter static directory (Enter to accept the default): " -i "$static_directory" static_directory
-customLog "Static directory is set to: $static_directory"
-
-# Write to .env File
-cat <<EOF >.env
-GITHUB_ACCESS_TOKEN=$github_token
-SERVER_PORT=$server_port
-STATIC_DIR=$static_directory
-EOF
-
-customLog ".env File is created."
-
 sudo apt update
 
 dependencies=("git" "docker.io" "docker-compose" "watch")
@@ -49,9 +31,26 @@ fi
 #   Download the latest version of the widget
 git clone -b latest https://github.com/imananoosheh/github-contributions-widget.git
 
-mv .env github-contributions-widget/.env
-
 cd github-contributions-widget
+
+
+github_token=''
+read -p "Enter your GitHub access token: " github_token
+server_port=3003
+read -e -p "Enter server port (Enter to accept the default): " -i "$server_port" server_port
+customLog "Server port is set to: $server_port"
+static_directory='/usr/src/app/public/'
+read -e -p "Enter static directory (Enter to accept the default): " -i "$static_directory" static_directory
+customLog "Static directory is set to: $static_directory"
+
+# Write to .env File
+cat <<EOF >.env
+GITHUB_ACCESS_TOKEN=$github_token
+SERVER_PORT=$server_port
+STATIC_DIR=$static_directory
+EOF
+
+customLog ".env File is created."
 
 #   Build the Docker image based on the latest version
 docker build \
@@ -59,6 +58,8 @@ docker build \
     --build-arg STATIC_DIR=$static_directory \
     -t github-contributions-widget .
 
+customLog "Docker Image is built."
+
 #   Run built image. It always restarts when failed.
 #   It watchs docker processes for status check purposes. Press 'q' to quit watch.
-docker run -p 3003:3003 -d --restart always --name gh-cal-widget github-contributions-widget && watch docker ps
+docker run -p $server_port:$server_port -d --restart always --name gh-cal-widget github-contributions-widget && watch docker ps
